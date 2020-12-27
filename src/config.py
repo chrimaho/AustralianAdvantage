fd190b7299f8ea0fbe172fdd53bda18ff43f6d0c
chrimaho <chrismahoney@hotmail.com>
2020-12-27 12:01:15 +1100

    Update config.py to sources.py

--- src/config.py
+++
@@ -1,3 +0,0 @@
-# Data by post code ----
-PostCode = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_POA/2773+2750+2148.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions&startPeriod=2016"
-
---
+++ src/sources.py
@@ -0,0 +1,23 @@
+# SEIFA by Postal Area Code ----
+PostalAreaCode = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_POA/2773+2750+2148.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+# PostalAreaCode = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_POA/.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+
+
+# SEIFA by Local Government Area ----
+LocalGovernmentArea = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_LGA/10750+10900+16350.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+# LocalGovernmentArea = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_LGA/.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+
+
+# SEIFA by State Suburb Code ----
+StateSuburbCode = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_SSC/10397+10407+11656.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+# StateSuburbCode = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_SSC/.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+
+
+# SEIFA by Statistical Area One ----
+StatisticalAreaOne = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_SA1/1100701+1+1100702.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+# StatisticalAreaOne = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_SA1/.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+
+
+# SEIFA by Statistical Area Two ----
+StatisticalAreaTwo = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_SA2/102011028+10201+102.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
+# StatisticalAreaTwo = "http://stat.data.abs.gov.au/sdmx-json/data/ABS_SEIFA2016_SA2/.IRSAD+IRSD+IER+IEO.SCORE+RWAR+RWA+RWAD+RWAP+RWSR+RWST+RWSD+RWSP+MINS+MAXS+URP/all?detail=Full&dimensionAtObservation=AllDimensions"
